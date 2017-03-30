df1dd2905ca5d5ca08b3c3549aa87bfd2e5831ba
zhedian <cooldeer@126.com>
2017-03-30 23:27:54 +0800

    CRUD _form bootstrapform simple_form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,50 @@
 class GroupsController < ApplicationController
+  before_action :set_group,  only: [:edit, :show, :update, :destroy]
+
   def index
     @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+
+
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+  def show
+
+  end
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_path, notice: "Delete"
+
+  end
+  private
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
