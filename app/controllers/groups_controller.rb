4f8b7eb21fd057b1d229a0fad9985d7dc5669a5a
zhedian <cooldeer@126.com>
2017-04-01 19:27:53 +0800

    I don't know

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -50,6 +50,7 @@ class GroupsController < ApplicationController
   def check_user
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
+    end 
   end
 
   def group_params
